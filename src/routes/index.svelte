b8d26cb934ca4c8ae0b0d590d7df7013242fa182
SophieHofer <89294877+SophieHofer@users.noreply.github.com>
2021-08-21 11:13:30 +0200

    first changes

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,9 @@
+<style>
+.text {
+  font-size: 40px;
+  color: green;
+}
+</style>
+
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p class="text">Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
